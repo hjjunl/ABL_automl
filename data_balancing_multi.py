4f8a22df9609ac104142b3fad7a9fda20bed7423
Jisu-Kim <rlawltn9408@gmail.com>
2022-06-16 17:57:09 +0900

    Update data_balancing_multi.py

--- data_balancing_multi.py
+++ data_balancing_multi.py
@@ -7,8 +7,11 @@ import os
 import pandas as pd
 import argparse
 import pickle
+from datetime import datetime
 
 warnings.filterwarnings(action='ignore')
+
+today = datetime.today().strftime("%Y%m%d")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("parser")
@@ -43,10 +46,10 @@ if __name__ == '__main__':
     #### data load ####
     print(f'***************  Load dataset ******************')
     if train.split('.')[1] == "csv":
-        data = pd.read_csv(f'{os.getcwd()}/data/preprocessed/{train}')
+        data = pd.read_csv(f'{os.getcwd()}/data/{today}/preprocessed/{train}')
 
     elif train.split('.')[1] == "pkl":
-        data = pd.read_pickle(f'{os.getcwd()}/data/preprocessed/{train}')
+        data = pd.read_pickle(f'{os.getcwd()}/data/{today}/preprocessed/{train}')
 
     #### Sampling ####
     print(f'***************  Sampling dataset ******************')
@@ -62,9 +65,9 @@ if __name__ == '__main__':
     df_sampling = pd.concat([X_sampling, y_df], axis=1)
     
     if train.split('.')[1] == "csv":
-        df_sampling.to_csv(f'{os.getcwd()}/data/preprocessed/{train}', index=False)
+        df_sampling.to_csv(f'{os.getcwd()}/data/{today}/preprocessed/{train}', index=False)
 
     elif train.split('.')[1] == "pkl":
-        df_sampling.to_pickle(f'{os.getcwd()}/data/preprocessed/{train}')
+        df_sampling.to_pickle(f'{os.getcwd()}/data/{today}/preprocessed/{train}')
 
     
