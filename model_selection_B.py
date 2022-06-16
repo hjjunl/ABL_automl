bf3dbc8670ddb43bb5b287aecd4d98f7fd1b6b5e
jisu <jisu@jisuui-MacBookAir.local>
2022-06-16 13:30:49 +0900

    Add plan B

---
+++ model_selection_B.py
@@ -0,0 +1,112 @@
+import imp
+import warnings
+import os
+
+import pandas as pd
+import argparse
+import pickle
+import json
+import time
+
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
+
+from sklearn.model_selection import KFold, cross_val_score
+
+from models.xgbmodel import XGBoostModel
+from models.lgbmmodel import LGBMModel
+from models.catboostmodel import CatBoostModel
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    parser = argparse.ArgumentParser("parser")
+    parser.add_argument('-train', dest ='train', default='train.csv') # 데이터명
+    parser.add_argument('-target', dest= "target", default = 'target') # 타겟 (이름 직접 지정해줘야함)
+
+    parser.add_argument('-valid_size', dest = 'valid_size', default = '0.2')
+    parser.add_argument('-model_save', dest = 'model_save', default = 'True')
+
+    parser.add_argument('-trials', dest = 'trials', default = '1')
+    
+    parser.add_argument('-gpu', dest = 'gpu', default = 'False')
+    parser.add_argument('-gpu_id', dest = 'gpu_id', default = '0')
+    parser.add_argument('-cpu_cnt', dest = 'cpu_cnt', default = '1')
+    
+    
+    args = parser.parse_args()
+    train = args.train
+    target = args.target
+
+    valid_size = args.valid_size
+    model_save = args.model_save
+
+    trials = args.trials
+    
+    gpu = args.gpu
+    gpu_id = args.gpu_id 
+    cpu_cnt = args.cpu_cnt
+    
+    #### data load ####
+    print(f'***************  Load dataset ******************')
+    if train.split('.')[1] == "csv":
+        data = pd.read_csv(f'{os.getcwd()}/data/preprocessed/{train}')
+
+    elif train.split('.')[1] == "pkl":
+        data = pd.read_pickle(f'{os.getcwd()}/data/preprocessed/{train}')
+
+
+    ### Best parameter search ####
+    X = data.drop(columns=[target])
+    y = data[[target]].values
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, stratify=y, test_size = float(valid_size), random_state = 42)
+
+    print(f'***************  XGB Best Parameter Search (Optuna) ******************')
+    xgb = XGBoostModel(X_train, y_train, X_valid, y_valid)
+    xgb.train(Optuna=True, opt_trials=int(trials), gpu=eval(gpu), gpu_id=int(gpu_id), cpu_cnt=int(cpu_cnt), save_model=eval(model_save), file_name='XGB')
+
+    print(f'***************  LGBM Best Parameter Search (Optuna) ******************')
+    lgb = LGBMModel(X_train, y_train, X_valid, y_valid)
+    lgb.train(Optuna=True, opt_trials=int(trials), gpu=eval(gpu), gpu_id=int(gpu_id), cpu_cnt=int(cpu_cnt), save_model=eval(model_save), file_name='LGBM')
+
+    print(f'***************  CatBoost Best Parameter Search (Optuna) ******************')
+    cbc = CatBoostModel(X_train, y_train, X_valid, y_valid)
+    cbc.train(Optuna=True, opt_trials=int(trials), gpu=eval(gpu), gpu_id=int(gpu_id), cpu_cnt=int(cpu_cnt), save_model=eval(model_save), file_name='CBC')
+
+
+
+    ### Model Selection ###
+    with open('./outputs/xgboost/params/XGB_Optuna.json','r') as fp:
+        xgb_params = json.load(fp)
+
+    with open('./outputs/lightgbm/params/LGBM_Optuna.json','r') as fp:
+        lgb_params = json.load(fp)
+
+    with open('./outputs/catboost/params/CBC_Optuna.json','r') as fp:
+        cat_params = json.load(fp)
+
+    models = {'XGB': XGBClassifier(** xgb_params, n_jobs = int(cpu_cnt), gpu=eval(gpu), gpu_id=gpu_id), \
+            'LGBM': LGBMClassifier(** lgb_params, n_jobs = int(cpu_cnt)), 'CBC': CatBoostClassifier(** cat_params, logging_level='Silent', thread_count = int(cpu_cnt))}
+
+        
+    kfold = KFold(n_splits=int(cv), shuffle = True, random_state=42)
+    answer = []
+
+    for model in models.keys():
+        print(model)
+        scores = cross_val_score(models[model] , X, y, cv=kfold, scoring='neg_log_loss', n_jobs = int(cpu_cnt))
+        answer.append(scores)
+
+    cross_val_result = pd.DataFrame(answer)
+    cross_val_result['model'] = models.keys()
+    cross_val_result['mean'] = cross_val_result.mean(axis=1)
+        
+    cross_val_result = cross_val_result.sort_values('mean', ascending = False)
+    cross_val_result = cross_val_result.reset_index(drop=True)
+    best_model = cross_val_result['model'][0]
+            
+    print('*********************  Vanilla Model Cross Val *********************')
+    print('                                           ')
+    print(cross_val_result)
+    print('                                           ')
+    print(f'***************** Best Model from Vanilla CV : {best_model} ******************')
---
+++ total_script_B.py
@@ -0,0 +1,99 @@
+import re, os
+import subprocess
+import argparse
+
+
+def main(train, test, target, cv, sampling, sampling_ratio, sampling_k_neighbors, sampling_m_neighbors, smotesvm_stepsize, categorical_features_index, \
+        model_type, large_data, valid_size, model_save, trials, gpu, gpu_id, cpu_cnt):
+    # try:
+    #     print("============================= Start get_dataset.py =============================")
+    #     subprocess.run(['python', 'get_dataset.py', '-data', data])
+    # except Exception as e:
+    #     print("Get data ERROR!!!")
+    #     print("ERROR Code: ", e)
+    # else:
+    #     print("============================= Finish get_dataset.py =============================")
+    
+
+    # try:
+    #     print("============================= Start Data Balancing (binary) =============================")
+    #     subprocess.run(['python', 'data_balancing_binary.py', '-train', train, '-target', target, '-sampling', sampling, \
+    #                         '-sampling_ratio', sampling_ratio, '-sampling_k_neighbors', sampling_k_neighbors, '-sampling_m_neighbors', sampling_m_neighbors, \
+    #                             '-smotesvm_stepsize', smotesvm_stepsize, '-categorical_features_index', categorical_features_index, '-cpu_cnt', cpu_cnt])
+    # except Exception as e:
+    #     print("Data Balancing ERROR!!!")
+    #     print("ERROR Code: ", e)
+    # else:
+    #     print("============================= Finish Data Balancing =============================")
+
+
+    try:
+        print("============================= Start Data Balancing (multi) =============================")
+        subprocess.run(['python', 'data_balancing_multi.py', '-train', train, '-target', target, '-sampling', sampling, \
+                            '-sampling_ratio', sampling_ratio, '-sampling_k_neighbors', sampling_k_neighbors, '-sampling_m_neighbors', sampling_m_neighbors, \
+                                '-smotesvm_stepsize', smotesvm_stepsize, '-categorical_features_index', categorical_features_index, '-cpu_cnt', cpu_cnt])
+    except Exception as e:
+        print("Data Balancing ERROR!!!")
+        print("ERROR Code: ", e)
+    else:
+        print("============================= Finish Data Balancing =============================")
+
+
+
+    try:
+        print("============================= Start Parameter tunning & Model Selection =============================")
+        subprocess.run(['python', 'model_selection_B.py', '-train', train, '-target', target, '-cv', cv, '-gpu', gpu, '-gpu_id', gpu_id,\
+                        '-cpu_cnt', cpu_cnt])
+    except Exception as e:
+        print("Model Selection ERROR!!!")
+        print("ERROR Code: ", e)
+    else:
+        print("============================= Finish Parameter tunning & Model Selection =============================")
+
+    try:
+        print("============================= Start XAI =============================")
+        subprocess.run(['python', 'xai_results.py', '-test', test, '-target', target, '-model', model_type, \
+                            '-large_data', large_data, '-gpu', gpu, '-gpu_id', gpu_id, '-cpu_cnt', cpu_cnt])
+    except Exception as e:
+        print("XAI ERROR!!!")
+        print("ERROR Code: ", e)
+    else:
+        print("============================= Finish XAI =============================")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run total AutoML script")
+    parser.add_argument('-train', '--train', type=str, help='Data set (defult: train.csv)', default = 'train.csv', required=False)
+    parser.add_argument('-test', '--test', type=str, help='Data set (defult: test.csv)', default = 'test.csv', required=False)
+    parser.add_argument('-target', '--target', type=str, help='Target column (defult: target)', default = 'target', required=False)
+    
+    parser.add_argument('-cv', '--cv', type=str, help='The number of cross Validation', default = '5', required=False)
+
+    parser.add_argument('-sampling', "--sampling", type=str, help='Sampling method (Options: RandomUnderSample(default), RandomOverSample, Smote, SmoteNC, SmoteSVM, ADASYN)', default = 'RandomUnderSample', required=False)
+    parser.add_argument('-sampling_ratio', "--sampling_ratio", type=str, help='Sampling ratio', default = '0.5', required=False)
+    parser.add_argument('-sampling_k_neighbors', "--sampling_k_neighbors", type=str, help='K neighbors', default = '5', required=False)
+    parser.add_argument('-sampling_m_neighbors', "--sampling_m_neighbors", type=str, help='M neighbors', default = '10', required=False)
+    parser.add_argument('-smotesvm_stepsize', "--smotesvm_stepsize", type=str, help='smotesvm stepsize', default = '0.5', required=False)
+    parser.add_argument('-categorical_features_index', "--categorical_features_index", type=str, help='Categorical features index', default = 'None', required=False)
+
+
+    parser.add_argument('-model_type', '--model_type', type=str, help='Select Model Type (options: XGB(default), LGBM, CBC)', default = 'XGB', required=False)
+
+    parser.add_argument('-large_data', '--large_data', type=str, help='Large data or not (Options: True, False(default))', default = 'False', required=False)
+    parser.add_argument('-valid_size', '--valid_size', type=str, help='Valid size for train/valid split (default: 0.2)', default = '0.2', required=False)
+    parser.add_argument('-model_save', '--model_save', type=str, help='Model save or not (Options: True(default), False)', default = 'True', required=False)
+    parser.add_argument('-trials', '--trials', type=str, help='Trials for optuna (default: 1)', default = '5', required=False)
+
+    parser.add_argument('-gpu', '--gpu', type=str, help='GPU setting (default: False)', default = 'False', required=False)
+    parser.add_argument('-gpu_id', '--gpu_id', type=str, help='GPU id (default: 0)', default = '0', required=False)
+    parser.add_argument('-cpu_cnt', '--cpu_cnt', type=str, help='The number of cpu count for train model (defult: 1)', default = '1', required=False)
+
+
+    args = parser.parse_args()
+
+    return args.train, args.test, args.target, args.cv, args.sampling, args.sampling_ratio, \
+        args.sampling_k_neighbors, args.sampling_m_neighbors, args.smotesvm_stepsize, args.categorical_features_index,\
+        args.model_type, args.large_data, args.valid_size, args.model_save, args.trials, args.gpu, args.gpu_id, args.cpu_cnt
+
+if __name__ == '__main__':
+    main(*parse_args())
